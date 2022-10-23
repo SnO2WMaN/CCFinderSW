41964c8e008807735d2f2e75f65ebd5d7c790079
SnO2WMaN <me@sno2wman.net>
2022-10-23 11:06:10 +0900

    build distribution with nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nix-filter.url = "github:numtide/nix-filter";
   };
   # dev
   inputs = {
@@ -20,14 +21,44 @@
         pkgs = import inputs.nixpkgs {
           inherit system;
           overlays = with inputs; [
+            nix-filter.overlays.default
             devshell.overlay
           ];
         };
       in {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "CCFinderSW-build";
+          version = "1.0";
+          src = pkgs.nix-filter {
+            root = ./.;
+            include = with pkgs.nix-filter; [
+              "build.gradle"
+              "settings.gradle"
+              (inDirectory "src")
+              (inDirectory "lib")
+            ];
+          };
+
+          nativeBuildInputs = with pkgs; [
+            unzip
+            gradle_4
+          ];
+          buildInputs = with pkgs; [
+            jdk8
+          ];
+          buildPhase = ''
+            gradle distZip
+          '';
+          installPhase = ''
+            unzip ./build/distributions/CCFinderSW-1.0.zip
+            cp -r ./CCFinderSW-1.0 $out
+          '';
+        };
+
         devShells.default = pkgs.devshell.mkShell {
           packages = (
             with pkgs; [
-              jre8
+              jdk8
               gradle_4
             ]
           );
